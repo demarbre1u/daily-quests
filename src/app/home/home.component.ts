6181693ef73aa132eed406f81d0ec45d75f1d450
Allan Demarbre <ademarbre@groupe-cac.com>
2020-02-05 12:56:12 +0100

    Quests are now loaded from a JSON file

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as Quests from './../../assets/quests/quests.json';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  public static DAILY_QUEST_NUMBER = 3
+  private quests = Quests.quests
+  private dailyQuests = []
+
+  constructor() {}
+
+  ngOnInit() {
+    this.dailyQuests = this.pickDailyQuests()
+    console.log(this.dailyQuests)
+  }
+
+  // Picks DAILY_QUEST_NUMBER quests and add it to the daily quests
+  pickDailyQuests()
+  {
+    let pickedQuests = []
+    for(let i = 0; i < HomeComponent.DAILY_QUEST_NUMBER; i++)
+    {
+      let randomIndex = Math.round(Math.random() * (this.quests.length - 1))
+
+      pickedQuests.push(this.quests[randomIndex])
+    }
+    
+    return pickedQuests
+  }
+}
