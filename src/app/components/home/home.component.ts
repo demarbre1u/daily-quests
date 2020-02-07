fa359c078e5b175eb4024e8675869415ccd93beb
Allan Demarbre <ademarbre@groupe-cac.com>
2020-02-07 15:51:42 +0100

    Design of home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,7 +5,7 @@ import { QuestsService } from 'src/app/service/quests/quests.service.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'], 
 })
 export class HomeComponent implements OnInit {
 
