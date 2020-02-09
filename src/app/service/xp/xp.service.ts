e495ea843db68082fc0ced01b3d4d827306a0f43
Demarbre Allan <demarbreallan@gmail.com>
2020-02-09 21:51:30 +0100

    Refactoring + adding comments

--- src/app/service/xp/xp.service.ts
+++ src/app/service/xp/xp.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 import * as xpTable from '../../../assets/data/xp_table.json';
@@ -8,6 +8,13 @@ import * as statsTable from '../../../assets/data/stats_table.json';
   providedIn: 'root'
 })
 export class XpService {
+
+  // The default stats if the player doesn't have any yet
+  private defaultStatsObject = {
+    strengh: 0, 
+    intelligence: 0, 
+    wisdom: 0
+  }
 
   private xpChangedSource = new Subject<any>()
   xpChanged$ = this.xpChangedSource.asObservable()
@@ -24,20 +31,28 @@ export class XpService {
   }
 
   // Calculates the current level of the player based on their total XP
+  calculateData()
+  {
+    this.calculateLevelData()
+    this.calculateStatsData()
+  }
+
+  // Calculates the data related to the XP in general
   calculateLevelData()
   {
     let currentXP = this.getXP()
     let xpRange = this.xpTable.filter(e => currentXP >= e.from && currentXP < e.to)[0]
     let xpNeeded = xpRange.to - xpRange.from
     let currentLvl = xpRange.level
-
     currentXP -= xpRange.from
-
     let percentage = Math.round(currentXP / xpNeeded * 100)
 
     this.xpChangedSource.next({currentLvl, currentXP, xpNeeded, percentage})
+  }
 
-
+  // Calculates the data related to the stats
+  calculateStatsData()
+  {
     let currentStats = this.getStats()
     let newStats = {}
     for(let stats of Object.keys(currentStats))
@@ -64,7 +79,7 @@ export class XpService {
   {
     let stats = JSON.parse(localStorage.getItem('stats'))
 
-    return stats ? stats : {strengh: 0, intelligence: 0, wisdom: 0}
+    return stats ? stats : this.defaultStatsObject
   }
 
   // Add a given amount of XP to the total XP of the player
@@ -74,6 +89,7 @@ export class XpService {
     let currentXP = this.getXP() + xp
     localStorage.setItem('xp', currentXP)
 
+    // We add the XP also in the given stat
     let type = quest.type
     let stats = this.getStats()
     stats[type] += xp
